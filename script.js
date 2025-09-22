9b8bcf92ddacee1053fea58afe7821030e90a900
11pepi <elevenpepsiman@outlook.com>
2025-09-22 17:41:31 -0400

    lol

--- script.js
+++ script.js
@@ -2,19 +2,21 @@ async function getLatestCratesIoVersion(crateName) {
   try {
     const response = await fetch(`https://crates.io/api/v1/crates/${crateName}`);
     if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
+      throw new Error(`AAAAA HELP ME heres sum info: ${response.status}`);
     }
     const data = await response.json();
     return data.crate.max_version;
   } catch (error) {
-    console.error("Error fetching crate information:", error);
+    console.error("no u:", error);
     return null;
   }
 }
 
 getLatestCratesIoVersion("ospl").then(version => {
   if (version) {
-    document.getElementById("latest-version").innerHTML = `<a href="https://crates.io/crates/ospl">${version}</a>`;
+    //document.getElementById("latest-version").innerHTML = `<a href="https://crates.io/crates/ospl"><code>${version}</code></a>`;
+    document.getElementById("latest-version").setAttribute("href", "https://crates.io/crates/ospl");
+    document.getElementById("latest-version").firstChild.innerText = version;
   } else {
     document.getElementById("latest-version").innerHTML = "Could not retreive the latest version of OSPL from <a href=https://crates.io/crates/ospl>crates.io</a>.";
   }
